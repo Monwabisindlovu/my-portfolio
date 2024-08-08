667164f54c1ad4f47426461bfec7672448fba601
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-08-08 13:27:40 +0200

    updating my cv and cv coverlatter links

--- src/components/CV.js
+++ src/components/CV.js
@@ -5,10 +5,10 @@ const CV = () => (
   <section id="cv">
     <h2>Curriculum Vitae</h2>
     <p>Click the link below to download my CV:</p>
-    <a href="/path-to-your-cv.pdf" download>Download CV</a>
+    <a href="https://1drv.ms/b/s!AhRkYglczgK8wzYoMDlh3ht6wzqz?e=UMzCef" download>Download CV</a>
     <h2>Cover Letter</h2>
     <p>Click the link below to download my cover letter:</p>
-    <a href="/path-to-your-cover-letter.pdf" download>Download Cover Letter</a>
+    <a href="https://1drv.ms/b/s!AhRkYglczgK8w0tDC6ffJdydgSL-?e=evPSuc" download>Download Cover Letter</a>
   </section>
 );
 
