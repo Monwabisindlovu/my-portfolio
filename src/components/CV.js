208edcf0aefb0c2f4381f23c9ff789537a246734
monwabisindlovu <monwabisindlovu78@gmail.com>
2024-08-08 13:44:20 +0200

    cv file

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,14 +1,13 @@
-// src/components/CV.js
 import React from 'react';
 
 const CV = () => (
   <section id="cv">
     <h2>Curriculum Vitae</h2>
     <p>Click the link below to download my CV:</p>
-    <a href="https://1drv.ms/b/s!AhRkYglczgK8wzYoMDlh3ht6wzqz?e=UMzCef" download>Download CV</a>
+    <a href="https://1drv.ms/b/s!AhRkYglczgK8wzYoMDlh3ht6wzqz?e=UMzCef">Download CV</a>
     <h2>Cover Letter</h2>
     <p>Click the link below to download my cover letter:</p>
-    <a href="https://1drv.ms/b/s!AhRkYglczgK8w0tDC6ffJdydgSL-?e=evPSuc" download>Download Cover Letter</a>
+    <a href="https://1drv.ms/b/s!AhRkYglczgK8w0tDC6ffJdydgSL-?e=evPSuc">Download Cover Letter</a>
   </section>
 );
 
