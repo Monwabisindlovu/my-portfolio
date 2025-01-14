bdca4c872808c66753672f5b332809bd8e3f77b5
monwabisi <122863839+Monwabisindlovu@users.noreply.github.com>
2025-01-14 11:55:33 +0200

    Update CV.js

--- src/components/CV.js
+++ src/components/CV.js
@@ -4,7 +4,7 @@ const CV = () => (
   <section id="cv">
     <h2>Curriculum Vitae</h2>
     <p>Click the link below to download my CV:</p>
-    <a href="https://1drv.ms/b/s!AhRkYglczgK8wzYoMDlh3ht6wzqz?e=UMzCef">Download CV</a>
+    <a href="https://1drv.ms/b/c/bc02ce5c09626414/EUVBq8jR35pKmQgK5HIER-YB7OcbgPAAtZFvAFipCdYrgw?e=okja0r">Download CV</a>
     <h2>Cover Letter</h2>
     <p>Click the link below to download my cover letter:</p>
     <a href="https://1drv.ms/b/s!AhRkYglczgK8w0tDC6ffJdydgSL-?e=evPSuc">Download Cover Letter</a>
