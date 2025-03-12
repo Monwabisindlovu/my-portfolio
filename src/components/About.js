8cdf94e79977c080323c3d059804e937018ab1e4
monwabisi <122863839+Monwabisindlovu@users.noreply.github.com>
2025-03-12 18:17:35 +0200

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ import React from 'react';
 const About = () => (
   <section id="about">
     <h2>About</h2>
-    <p>I am a highly skilled software engineer with extensive expertise in C, Node.js, Python, and JavaScript. I specialize in developing scalable, efficient software solutions that address complex challenges and drive technological innovation.</p>
+    <p>I am a highly skilled software engineer with extensive expertise in C, Reactjs ,Node.js, Python, and JavaScript. I specialize in developing scalable, efficient software solutions that address complex challenges and drive technological innovation.</p>
   </section>
 );
 
